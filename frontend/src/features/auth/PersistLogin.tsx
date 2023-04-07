e18b7bc0d4d4ed04806d82062b3ce110cef49d50
Witold <witekrychlik@o2.pl>
2023-04-07 07:50:56 +0200

    prepared to deploy

--- frontend/src/features/auth/PersistLogin.tsx
+++ frontend/src/features/auth/PersistLogin.tsx
@@ -17,7 +17,7 @@ const PersistLogin = () => {
     }] = useRefreshMutation()   
 
     useEffect(() => {
-      if (effectRan.current === true) {
+      if (effectRan.current === true || process.env.NODE_ENV !== 'development') {
         const verifyRefreshToken = async () => {
           try {
               await refresh('')
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 import { Provider } from "react-redux"
 import { store } from "./app/store"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
